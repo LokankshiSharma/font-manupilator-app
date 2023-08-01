56681d36ee61f1a994b19e4c1dfef7b67a40d281
LokankshiSharma <81632228+LokankshiSharma@users.noreply.github.com>
2023-08-01 19:05:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 function setup(){
     canvas=createCanvas(500, 500);
- canvas.center();
-      video=createCapture( VIDEO );
+  canvas.position(600,90);
+  video=createCapture( VIDEO );
   video.size(500, 500);
   PoseNet=ml5.poseNet(video, modelLoaded);
   PoseNet.on("pose", gotResult)
-  poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
   }
   function draw(){
     textSize(difference);
     fill('black');
     text('Hello World', 50, 400);
+    background("cadetblue");
   }
   function gotPoses(results){
     if(results.length > 0){
@@ -20,7 +19,6 @@ function setup(){
     rightWristX= results[0].pose.rightWrist.x;
     difference = floor (leftWristX - rightWristX);  
   }
-}
   function modelLoaded(){
     console.log("PoseNet is initialized");
   }
